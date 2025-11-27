efb7819f2a4688597aa4551af9243e85cf49aeb0
Phann Pha <phannpha.fa@gmail.com>
2025-11-27 21:27:33 +0700

    created simple api endpoint

--- core/network-client/retrofit-client/src/main/java/com/domain/visor/school/retrofit_client/services/APIInterface.kt
+++ core/network-client/retrofit-client/src/main/java/com/domain/visor/school/retrofit_client/services/APIInterface.kt
@@ -1,5 +1,11 @@
 package com.domain.visor.school.retrofit_client.services
 
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface APIInterface {
 
+    @GET(value = "path/to/endpoint")
+    suspend fun onGetUser(): Response<ResponseBody>
 }
