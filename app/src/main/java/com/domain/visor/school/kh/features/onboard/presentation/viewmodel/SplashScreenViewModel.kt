ee0f3490dc3e33d9c32e38e2073a558145bbccd7
Phann Pha <phannpha.fa@gmail.com>
2025-12-16 19:33:03 +0700

    splash screen

--- app/src/main/java/com/domain/visor/school/kh/features/onboard/presentation/viewmodel/SplashScreenViewModel.kt
+++ app/src/main/java/com/domain/visor/school/kh/features/onboard/presentation/viewmodel/SplashScreenViewModel.kt
@@ -1,14 +1,13 @@
 package com.domain.visor.school.kh.features.onboard.presentation.viewmodel
 
-import android.app.Activity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,19 +16,11 @@ class SplashScreenViewModel @Inject constructor() : ViewModel() {
     private val _uiState:  MutableLiveData<String> = MutableLiveData()
     val uiState: LiveData<String> = _uiState
     
-    fun onAsyncDataInfo(activity: Activity) {
+    fun onAsyncDataInfo() {
         viewModelScope.launch {
-            configure(activity = activity, tag = "km")
-            _uiState.postValue("km")
+            delay(timeMillis = 1500L)
+            _uiState.postValue("")
         }
-    }
-    
-    private fun configure(activity: Activity, tag: String) {
-        val config = activity.resources.configuration
-        val local = Locale(tag)
-        Locale.setDefault(local)
-        config.setLocale(local)
-        activity.createConfigurationContext(config)
     }
     
     override fun onCleared() {
