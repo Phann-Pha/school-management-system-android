6c66dda7dc694d12ec7cdfb7a8adea2bc67e5bed
Phann Pha <phannpha.fa@gmail.com>
2025-12-17 20:38:36 +0700

    clean code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,6 @@ android {
         jvmTarget = JvmTarget.JVM_17.target
     }
     buildToolsVersion = "35.0.0"
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    androidResources {
-        generateLocaleConfig = true
-    }
 }
 
 dependencies {
