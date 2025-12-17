5a747ad855080171a6e04265fa995cacc2e2bbde
Phann Pha <phannpha.fa@gmail.com>
2025-12-17 21:25:14 +0700

    refactored screen ui

--- app/src/main/java/com/domain/visor/school/kh/features/onboard/presentation/view/SplashScreenActivity.kt
+++ app/src/main/java/com/domain/visor/school/kh/features/onboard/presentation/view/SplashScreenActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +20,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -33,11 +35,12 @@ import com.domain.visor.school.kh.features.language.presentation.view.SelectingL
 import com.domain.visor.school.kh.features.onboard.presentation.viewmodel.SplashScreenViewModel
 
 @SuppressLint("CustomSplashScreen")
-class SplashScreenActivity : BaseComponentActivity() {
-    
+class SplashScreenActivity : BaseComponentActivity()
+{
+
     private lateinit var activity: Activity
     private val viewmodel: SplashScreenViewModel by viewModels()
-    
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         activity = this@SplashScreenActivity
@@ -45,48 +48,58 @@ class SplashScreenActivity : BaseComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
-                Box(modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = colorResource(id = R.color.white))
-                ) {
-                    Column(modifier = Modifier
-                            .fillMaxSize()
-                            .padding(
-                                top = padding.calculateTopPadding(),
-                                bottom = padding.calculateBottomPadding()
-                            ),
-                        verticalArrangement = Arrangement.SpaceBetween,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Spacer(modifier = Modifier.size(size = 0.dp))
-                        Image(
-                            modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(height = 250.dp)
-                                    .background(color = colorResource(id = R.color.white)),
-                            painter = painterResource(id = R.drawable.onboard_3),
-                            contentDescription = null
-                        )
-                        Text(
-                            text =  stringResource(id = R.string.app_version),
-                            fontSize = 13.sp,
-                            color = colorResource(id = R.color.black),
-                            fontWeight = FontWeight.Normal
-                        )
-                    }
-                }
+                body(padding = padding)
             }
         }
 
         onSyncDataInfo()
         onObservableViewModel()
     }
-    
-    private fun onSyncDataInfo() {
+
+    @Composable
+    private fun body(padding: PaddingValues)
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.white))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = padding.calculateTopPadding(),
+                        bottom = padding.calculateBottomPadding()
+                    ),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.size(size = 0.dp))
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(height = 250.dp)
+                        .background(color = colorResource(id = R.color.white)),
+                    painter = painterResource(id = R.drawable.onboard_3),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(id = R.string.app_version),
+                    fontSize = 13.sp,
+                    color = colorResource(id = R.color.black),
+                    fontWeight = FontWeight.Normal
+                )
+            }
+        }
+    }
+
+    private fun onSyncDataInfo()
+    {
         viewmodel.onAsyncDataInfo()
     }
-    
-    private fun onObservableViewModel() {
+
+    private fun onObservableViewModel()
+    {
         viewmodel.uiState.observe(this) {
             startActivity(SelectingLanguageScreenActivity.onNewInstance(activity = activity, lang = it))
             activity.finish()
