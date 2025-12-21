b5cc3125b4af2820a688597709deafec9c7cd8d0
Phann Pha <phannpha.fa@gmail.com>
2025-12-21 14:13:49 +0700

    rollback http interceptor

--- core/network-client/retrofit-client/src/main/java/com/domain/visor/school/retrofit_client/interceptor/HttpInterceptor.kt
+++ core/network-client/retrofit-client/src/main/java/com/domain/visor/school/retrofit_client/interceptor/HttpInterceptor.kt
@@ -1,15 +1,56 @@
 package com.domain.visor.school.retrofit_client.interceptor
 
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import retrofit2.Invocation
 
 class HttpInterceptor : Interceptor
 {
+    @Retention(value = AnnotationRetention.RUNTIME)
+    @Target(AnnotationTarget.FUNCTION)
+    annotation class Authorized
+
     override fun intercept(chain: Interceptor.Chain): Response
     {
-        val request = chain.request()
+        var request = chain.request()
+        val invocation = chain.request().tag(Invocation::class.java) ?: return chain.proceed(chain.request())
+        containedOnInvocation(invocation).forEach { annotation ->
+
+            val key = "Authorization"
+            val value = "Bearer your_token_here"
+            request = onHandleAnnotation(annotation = annotation, request = request, key = key, value = value)
+        }
+
+        // process request
         val response = chain.proceed(request)
+        if (response.code == 503)
+        {
+
+        }
 
         return response
     }
+
+    private fun containedOnInvocation(invocation: Invocation): Set<Annotation>
+    {
+        return invocation.method().annotations.toSet()
+    }
+
+    private fun onHandleAnnotation(annotation: Annotation, request: Request, key: String, value: String): Request
+    {
+        return when (annotation)
+        {
+            is Authorized -> addAuthHeader(request = request, key = key, value = value)
+            else          -> request
+        }
+    }
+
+    private fun addAuthHeader(request: Request, key: String, value: String): Request
+    {
+        return request.newBuilder()
+            .addHeader(name = key, value = value)
+            .build()
+    }
 }
+
